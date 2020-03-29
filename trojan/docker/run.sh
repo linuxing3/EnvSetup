24ec173f485cb59ea00227f3555ec785ffda3d5b
linuxing3 <linuxing3@qq.com>
2020-03-29 23:17:11 +0000

    modulized folders

--- trojan/docker/run.sh
+++ trojan/docker/run.sh
@@ -1,2 +1,2 @@
-docker run -d --restart=unless-stopped -e DOMAIN=dongxishijie.xyz -e PASSWD="mm123456 user2"-v /root/EnvSetup/trojan:/root/conf -p 443:443 -p 80:80 --name trojan lihaixin/trojan-docker
+docker run -d --restart=unless-stopped -e DOMAIN=dongxishijie.xyz -e PASSWD="mm123456"-v /root/EnvSetup/trojan/docker:/root/conf -p 4433:4434 -name trojan lihaixin/trojan-docker
 
