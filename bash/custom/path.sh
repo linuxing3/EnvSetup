4786114ab473c9cc09af16ba5d9848df28a545b8
linuxing3 <linuxing3@qq.com>
2020-04-17 08:31:04 -0400

    20200417_1587126664

--- bash/custom/path.sh
+++ bash/custom/path.sh
@@ -1,15 +1,21 @@
 
-if [[ ! "$PATH" == "*~/EnvSetup/bash/bin*" ]]; then
-    export PATH="~/EnvSetup/bash/bin:$PATH"
+# set PATH so it includes user's bash custom bin if it exists
+if [[ ! "$PATH" == "*/EnvSetup/bash/bin*" ]]; then
+    export PATH="$HOME/EnvSetup/bash/bin:$PATH"
 fi
 
 # set PATH so it includes user's private bin if it exists
-if [[ ! "$PATH" == "*~/bin*" ]]; then
-    export PATH="~/bin:$PATH"
+if [[ ! "$PATH" == "*/EnvSetup/bin*" ]]; then
+    export PATH="$HOME/bin:$PATH"
 fi
 
-# set PATH so it includes user's private bin if it exists
-if [[ ! "$PATH" == "*~/.local/bin*" ]]; then
-    export PATH="~/.local/bin:$PATH"
+# set PATH so it includes user's local private bin if it exists
+if [[ ! "$PATH" == "*/EnvSetup/.local/bin*" ]]; then
+    export PATH="$HOME/.local/bin:$PATH"
+fi
+
+# set PATH so it includes user's dropbox bin if it exists
+if [[ ! "$PATH" == "*/EnvSetup/Dropbox/bin*" ]]; then
+    export PATH="$HOME/Dropbox/bin:$PATH"
 fi
 
