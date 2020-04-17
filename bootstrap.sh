d7bc36fb483ad6c5c9be60983e7a5d8a0d303c0d
linuxing3 <linuxing3@qq.com>
2020-04-16 21:43:45 -0400

    20200416_1587087824

--- bootstrap.sh
+++ bootstrap.sh
@@ -54,6 +54,9 @@ export LANG=en_US.UTF-8
 if [ -d "\$HOME/bin" ] ; then
     PATH="\$HOME/bin:\$PATH"
 fi
+if [ -d "\$HOME/.local/bin" ] ; then
+    PATH="\$HOME/.local/bin:\$PATH"
+fi
 if [ -d "\$HOME/EnvSetup/bash/bin" ] ; then
     PATH="\$HOME/EnvSetup/bash/bin:\$PATH"
 fi
