cc090f9f909d0a747d9b057e9a1d35db51b3f08a
linuxing3 <linuxing3@qq.com>
2020-04-20 23:44:01 -0400

    20200420_1587440641

--- bash/bin/setup-wifi-debian.sh
+++ bash/bin/setup-wifi-debian.sh
@@ -1,9 +1,15 @@
+#! /usr/bin/env bash
+
 ip link set wlp5s0 up
 
 cat << EOF >> /etc/network/interfaces
 
 auto wlp5s0
-iface wlp5s0 inet dhcp
+iface wlp5s0 inet static
+address 192.168.1.166
+netmask 255.255.255.0
+gateway 192.168.1.1
+nameserver 80.80.80.80, 80.80.81.81
 pre-up ip link set wlp5s0 up
 pre-up iwconfig wlp5s0 essid ssid
 wpa-ssid dulce
@@ -11,5 +17,3 @@ wpa-psk 20dulce17
 EOF
 
 ifup wlp5s0
-#1587430788
-vim .bash_history 
