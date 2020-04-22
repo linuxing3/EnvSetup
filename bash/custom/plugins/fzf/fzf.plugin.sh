d64a3a2241e85ff27e4d5b20b7f34a7c0f8f4110
linuxing3 <linuxing3@qq.com>
2020-04-22 21:46:25 +0000

    Commited by vagrant from /home/vagrant/EnvSetup on 2020-04-22-21-46

--- bash/custom/plugins/fzf/fzf.plugin.sh
+++ bash/custom/plugins/fzf/fzf.plugin.sh
@@ -2,8 +2,8 @@
 
 export FZF_DEFAULT_COMMAND='fdfind --type f'
 
-export FZF_DEFAULT_OPTS="--height 40% --layout reverse \
---preview 'bat --style=numbers --color=always {} || file {}' "
+export FZF_DEFAULT_OPTS="--height 40% --layout reverse --border\
+--preview 'bat --style=numbers --color=always {} || cat {} || file {} || head -100 {}' "
 
 # Use ~~ as the trigger sequence instead of the default **
 export FZF_COMPLETION_TRIGGER='~~'
