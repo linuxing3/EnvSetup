f7ce398e9e822007c8afa6e38f99cf2e1acfced3
linuxing3 <linuxing3@qq.com>
2020-04-22 18:23:19 +0000

    Commited by vagrant from /home/vagrant/EnvSetup on 2020-04-22-18-23

--- bash/bin/bash.sh
+++ bash/bin/bash.sh
@@ -4,3 +4,11 @@ echo "==========================================================="
 # using oh-my-bash
 cd
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmybash/oh-my-bash/master/tools/install.sh)"
+
+echo "==========================================================="
+echo "installing bash-it"
+echo "==========================================================="
+mkdir -p ~/workspace
+cd ~/workspace
+git clone https://github.com/Bash-it/bash-it
+cd
