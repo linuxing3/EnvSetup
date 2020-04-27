f85a5a59a3a839a60c679e465b0269b015937c4f
linuxing3 <linuxing3@qq.com>
2020-04-27 17:39:21 +0000

    alias of git

--- bash/custom/plugins/emacs/emacs.plugin.sh
+++ bash/custom/plugins/emacs/emacs.plugin.sh
@@ -2,3 +2,13 @@
 
 alias es="emacs --daemon"
 alias ec="emacsclient -c"
+alias eq="emacs -q"
+
+doomx() {
+    export PATH=$PATH:~/.emacs.d/bin
+    doom
+}
+
+eb() {
+    emacs -q --batch -l ~/.emacs.editor --eval="$1"
+}
