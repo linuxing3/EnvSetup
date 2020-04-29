1bc981a64ede5087b45170f4abeeb0f0264dd468
linuxing3 <linuxing3@qq.com>
2020-04-29 17:24:01 +0000

    Commited by vagrant from /home/vagrant/EnvSetup on 2020-04-29-17-24

--- bash/bin/install-nps.sh
+++ bash/bin/install-nps.sh
@@ -4,8 +4,8 @@ a=""
 a=$(arch)
 if [[ "${a}" ==  "x86_64" ]]; then 
   version="amd64" 
-elif [[ "${a}" == "x86" ]]; then
-  version="i386"
+elif [[ "${a}" == "i686" ]]; then
+  version="386"
 elif [[ "${a}" == "armv7l" ]]; then
   version="arm_v7"
 fi
@@ -68,102 +68,42 @@ install_npc() {
 	wget "https://github.com/ehang-io/nps/releases/download/v0.26.6/linux_${version}_client.tar.gz"
 	tar xvf "linux_${version}_client.tar.gz"
 
-
 	touch run-npc
 	chmod +x run-npc
 	cat >> run-npc << EOF
 cd ~/npc
 nohup ./npc &
 EOF
-	rm conf/npc.conf
+	mv conf/npc.conf conf/npc.default.conf
 	cat > conf/npc.conf << EOF
 [common]
 server_addr=35.235.80.5:8024
 conn_type=tcp
-vkey=91rjxgre6mross98
+vkey=qqyzkzldrxycbdzwwsgh
 auto_reconnection=true
 max_conn=1000
 flow_limit=1000
 rate_limit=1000
-basic_username=11
-basic_password=3
+basic_username=978
+basic_password=916
 web_username=user
-web_password=1234
+web_password=mm123456
 crypt=true
 compress=true
-#pprof_addr=0.0.0.0:9999
-
-[health_check_test1]
-health_check_timeout=1
-health_check_max_failed=3
-health_check_interval=1
-health_http_url=/
-health_check_type=http
-health_check_target=127.0.0.1:80
-
-[health_check_test2]
-health_check_timeout=1
-health_check_max_failed=3
-health_check_interval=1
-health_check_type=tcp
-health_check_target=127.0.0.1:80
-
-[web]
-host=nps.xunqinji.top
-target_addr=127.0.0.1:80
 
 [tcp]
 mode=tcp
-server_port=7000
+server_port=11115
 target_addr=127.0.0.1:22
-
-[socks5]
-mode=socks5
-server_port=19009
-multi_account=multi_account.conf
-
-[file]
-mode=file
-server_port=7500
-local_path=/home/pi/Downloads
-strip_pre=/web/
-
-[http]
-mode=httpProxy
-server_port=19004
-
-[udp]
-mode=udp
-server_port=12253
-target_addr=114.114.114.114:53
-
-[ssh_secret]
-mode=secret
-password=ssh2
-target_addr=192.168.1.1:22
-
-[ssh_p2p]
-mode=p2p
-password=ssh3
-
-[secret_ssh]
-local_port=2001
-password=ssh2
-
-[p2p_ssh]
-local_port=2002
-password=ssh3
-target_addr=192.168.1.1:22
 EOF
 
 	echo "Run npc in background"
-	./run-npc
-
+  sudo ./npc install -config /home/${whoami}/npc/conf/npc.conf
+  sudo systemctl enable Npc
 	echo "Testing npc is running"
-	netstat -nelp | grep npc
-	if [[ ! -z $? ]];then
-	  echo "Started client"
-	fi
+  sudo systemctl start Npc
+  sudo systemctl status Npc
+  echo "Checkout ~/npc/conf/npc.default.conf for more examples"
 }
 
 
