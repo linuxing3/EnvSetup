da27e5d1e92bc5b5c5ccc5e3363aae1c6f696443
linuxing3 <linuxing3@qq.com>
2020-05-06 00:57:12 -0400

    Commited by vagrant from /home/vagrant/EnvSetup on 2020-05-06-00:57

--- bash/custom/plugins/nvm/nvm.plugin.sh
+++ bash/custom/plugins/nvm/nvm.plugin.sh
@@ -4,4 +4,12 @@ nn() {
   export NVM_DIR="$HOME/.nvm"
   [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
   [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+  export NVM_NODEJS_ORG_MIRROR="https://nodejs.org/dist"
+}
+
+nnmirror() {
+  export NVM_DIR="$HOME/.nvm"
+  [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+  [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+  export NVM_NODEJS_ORG_MIRROR="https://unofficial-builds.nodejs.org/download/release"
 }
