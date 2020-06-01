63b3be7b4a55fd349e2c2b26cc24ed5997a054a2
linuxing3 <linuxing3@qq.com>
2020-06-01 16:11:20 -0400

    Commited by root from /root/EnvSetup on 2020-06-01-16:11

--- bash/custom/plugins/nvm/nvm.plugin.sh
+++ bash/custom/plugins/nvm/nvm.plugin.sh
@@ -1,10 +1,13 @@
 #!/usr/bin/env bash
 
+
 nnn() {
 	export NVM_DIR="$HOME/.nvm"
 	[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"                   # This loads nvm
 	[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion" # This loads nvm bash_completion
 	export NVM_NODEJS_ORG_MIRROR="https://nodejs.org/dist"
+  export DENO_INSTALL="$HOME/.deno"
+  export PATH="$DENO_INSTALL/bin:$PATH"
 }
 
 nnmirror() {
