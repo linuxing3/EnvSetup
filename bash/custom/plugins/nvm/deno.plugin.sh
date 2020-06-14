22c08e0a706c6d33db2ee1bbe7ded61428faed59
linuxing3 <linuxing3@qq.com>
2020-06-14 14:22:09 -0400

    bash plugin must have same name

--- bash/custom/plugins/nvm/deno.plugin.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-
-# Deno stuff
-export DENO_INSTALL="$HOME/.deno"
-export PATH="$DENO_INSTALL/bin:$PATH"
--- bash/custom/plugins/nvm/nvm.plugin.sh
+++ bash/custom/plugins/nvm/nvm.plugin.sh
@@ -4,8 +4,12 @@ nnmirror() {
 	export NVM_NODEJS_ORG_MIRROR="https://unofficial-builds.nodejs.org/download/release"
 }
 
-# np stuff
+# node stuff
 export NVM_NODEJS_ORG_MIRROR="https://nodejs.org/dist"
 export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"                   # This loads nvm
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion" # This loads nvm bash_completion
+
+# deno stuff
+export DENO_INSTALL="$HOME/.deno"
+export PATH="$DENO_INSTALL/bin:$PATH"
