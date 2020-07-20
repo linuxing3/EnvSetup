70866bac3a350bf56645c0eb3cb7b1bb28301a43
linuxing3 <linuxing3@qq.com>
2020-07-20 05:35:17 +0000

    Commited by ubuntu from /home/ubuntu/EnvSetup on 2020-07-20-05:35

--- bash/bin/install-acme.sh
+++ bash/bin/install-acme.sh
@@ -3,15 +3,11 @@
 read -p "请输入Domain:" domain
 
 #申请https证书
-systemctl stop caddy
-systemctl stop nginx
 curl https://get.acme.sh | sh
 
-if [ ! -d /etc/letsencrypt/$domain_ecc]:
-	mkdir /etc/letsencrypt/$domain_ecc
-fi
+mkdir "/etc/letsencrypt/$domain"
 
 ~/.acme.sh/acme.sh  --issue  -d $domain  --standalone
 ~/.acme.sh/acme.sh  --installcert  -d  $domain   \
-	--key-file   /etc/letsencrypt/$domain_ecc/private.key \
-	--fullchain-file  /etc/letsencrypt/$domain_ecc/fullchain.cer
+	--key-file   /etc/letsencrypt/$domain/private.key \
+	--fullchain-file  /etc/letsencrypt/$domain/fullchain.cer
