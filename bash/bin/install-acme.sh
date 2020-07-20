b1dbb0abe396ea7b5bc7f8e22c38ea0d0c0a9084
linuxing3 <linuxing3@qq.com>
2020-07-20 13:13:55 +0800

    Commited by root from /root/EnvSetup on 2020-07-20-13:13

--- bash/bin/install-acme.sh
+++ bash/bin/install-acme.sh
@@ -7,11 +7,11 @@ systemctl stop caddy
 systemctl stop nginx
 curl https://get.acme.sh | sh
 
-if [ ! -d /etc/letsencrypt/dongxishijie.xyz_ecc ]:
-	mkdir /etc/letsencrypt/dongxishijie.xyz_ecc
+if [ ! -d /etc/letsencrypt/$domain_ecc]:
+	mkdir /etc/letsencrypt/$domain_ecc
 fi
 
 ~/.acme.sh/acme.sh  --issue  -d $domain  --standalone
 ~/.acme.sh/acme.sh  --installcert  -d  $domain   \
-	--key-file   /etc/letsencrypt/dongxishijie.xyz_ecc/private.key \
-	--fullchain-file  /etc/letsencrypt/dongxishijie.xyz_ecc/fullchain.cer
+	--key-file   /etc/letsencrypt/$domain_ecc/private.key \
+	--fullchain-file  /etc/letsencrypt/$domain_ecc/fullchain.cer
