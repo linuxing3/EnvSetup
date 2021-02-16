5e0dcc60da957d5ba399ac7d29a9cd4542802021
linuxing3 <linuxing3@qq.com>
2021-02-16 10:51:10 -0400

    update ps setting

--- config/windows/env_setup_default.ps1
+++ config/windows/env_setup_default.ps1
@@ -4,14 +4,12 @@
 # Copyrigtht: MIT
 # Date: 2020-05-11
 #
-
-# Unblock-File -Path .\windows7_env_setup_packages.ps1
+# Unblock-File -Path %
 Write-Host "Set-ExecutionPolicy -ExecutionPolicy RemoteSigned -Scope LocalMachine"
 if (!([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator")) { Start-Process powershell.exe "-NoProfile -ExecutionPolicy Bypass -File `"$PSCommandPath`"" -Verb RunAs; exit }
 
-function Check-Command($cmdname) {
-    return [bool](Get-Command -Name $cmdname -ErrorAction SilentlyContinue)
-}
+$homedir=[System.Environment]::GetEnvironmentVariable('USERPROFILE') + '\'
+[System.Environment]::SetEnvironmentVariable('HOME', homedir,[System.EnvironmentVariableTarget]::Machine)
 
 # -----------------------------------------------------------------------------
 $computerName = Read-Host '输入新的计算机名称'
@@ -67,82 +65,6 @@ Write-Host ""
 Write-Host "设置Windows 10 开发者模式." -ForegroundColor Green
 Write-Host "------------------------------------" -ForegroundColor Green
 reg add "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\AppModelUnlock" /t REG_DWORD /f /v "AllowDevelopmentWithoutDevLicense" /d "1"
-# -----------------------------------------------------------------------------
-Write-Host ""
-Write-Host "启用远程桌面并设置防火墙" -ForegroundColor Green
-Write-Host "------------------------------------" -ForegroundColor Green
-Set-ItemProperty "HKLM:\SYSTEM\CurrentControlSet\Control\Terminal Server\" -Name "fDenyTSConnections" -Value 0
-Set-ItemProperty "HKLM:\SYSTEM\CurrentControlSet\Control\Terminal Server\WinStations\RDP-Tcp\" -Name "UserAuthentication" -Value 1
-Enable-NetFirewallRule -DisplayGroup "Remote Desktop"
-
-if (Check-Command -cmdname 'choco') {
-    Write-Host "Choco 已安装"
-}
-else {
-    Write-Host ""
-    Write-Host "安装Chocolate for Windows..." -ForegroundColor Green
-    Write-Host "------------------------------------" -ForegroundColor Green
-    Set-ExecutionPolicy Bypass -Scope Process -Force; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
-}
-
-Write-Host ""
-Write-Host "安装常用软件和应用" -ForegroundColor Green
-Write-Host "------------------------------------" -ForegroundColor Green
-Write-Host "[WARN] Ma de in China: some software like Google Chrome require the true Internet first" -ForegroundColor Yellow
-
-if (Check-Command -cmdname 'git') {
-    Write-Host "Git已安装，为你升级"
-    choco update git -y
-}
-else {
-    Write-Host ""
-    Write-Host "安装Git for Windows..." -ForegroundColor Green
-    choco install git -y
-}
-
-if (Check-Command -cmdname 'node') {
-    Write-Host "Node.js已安装，为你升级"
-    choco update nodejs -y
-}
-else {
-    Write-Host ""
-    Write-Host "安装Nodejs" -ForegroundColor Green
-    choco install nodejs -y
-}
-
-Write-Host ""
-Write-Host "安装常用软件, wget, curl, fd, repgrep, powershell, 7zip" -ForegroundColor Green
-choco install wget -y
-choco install curl -y
-choco install fd -y
-choco install ripgrep -y
-choco install powershell7 -y
-choco install 7zip.install -y
-choco install vscode -y
-choco install vscode-powershell -y
-
-Write-Host ""
-Write-Host "建议安装的常用软件, googlechrome等等" -ForegroundColor Green
-Write-Host "choco install googlechrome -y"
-Write-Host "choco install potplayer -y"
-Write-Host "choco install dotnetcore-sdk" -y
-Write-Host "choco install ffmpeg -y"
-Write-Host "choco install openssl.light" -y
-Write-Host "choco install vscode-csharp" -y
-Write-Host "choco install vscode-icons" -y
-Write-Host "choco install vscode-mssql" -y
-Write-Host "choco install sysinternals -y"
-Write-Host "choco install notepadplusplus.install" -y
-Write-Host "choco install dotpeek -y"
-Write-Host "choco install linqpad -y"
-Write-Host "choco install fiddler -y"
-Write-Host "choco install beyondcompare -y"
-Write-Host "choco install filezilla -y"
-Write-Host "choco install lightshot.install" -y
-Write-Host "choco install microsoft-teams".install -y
-Write-Host "choco install teamviewer -y"
-Write-Host "choco install github-desktop" -y
 
 Write-Host "------------------------------------" -ForegroundColor Green
-Read-Host -Prompt "安装完成，按回车重启系统！"
-Restart-Computer
+Read-Host -Prompt "安装完成！"
---
+++ config/windows/env_setup_path.ps1
@@ -0,0 +1,52 @@
+# !/usr/bin/env powershell
+#
+# Author: linuxing3<linuxing3@qq.com>
+# Copyrigtht: MIT
+# Date: 2020-05-11
+# Usage: Script to set environment variables and putty themes
+#
+
+
+# Unblock-File -Path %
+Write-Host "Set-ExecutionPolicy -ExecutionPolicy RemoteSigned -Scope LocalMachine"
+if (!([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator")) { Start-Process powershell.exe "-NoProfile -ExecutionPolicy Bypass -File `"$PSCommandPath`"" -Verb RunAs; exit }
+
+$homedir=[System.Environment]::GetEnvironmentVariable('USERPROFILE') + '\'
+[System.Environment]::SetEnvironmentVariable('HOME', homedir,[System.EnvironmentVariableTarget]::Machine)
+
+Write-Host "将为您设置系统可执行文件路径。如果出现错误，请先运行以下命令开启验证"
+
+# -----------------------------------------------------------------------------
+Write-Host ""
+Write-Host "设置GOPATH和GOROOT " $computerName  -ForegroundColor Yellow
+Write-Host "------------------------------------" -ForegroundColor Green
+Write-Host "SET [GOPATH] to" $homedir "go"  -ForegroundColor Yellow
+Write-Host "SET [GOROOT] to" $drive "go"  -ForegroundColor Yellow
+[System.Environment]::SetEnvironmentVariable('GOPATH', $homedir + 'go',[System.EnvironmentVariableTarget]::Machine)
+[System.Environment]::SetEnvironmentVariable('GOROOT', $drive + 'go',[System.EnvironmentVariableTarget]::Machine)
+
+# -----------------------------------------------------------------------------
+# 
+Write-Host "设置NVM Environment variables " $computerName  -ForegroundColor Yellow
+[System.Environment]::SetEnvironmentVariable('NVM_HOME', 'C:\lib\nvm', [System.EnvironmentVariableTarget]::Machine)
+[System.Environment]::SetEnvironmentVariable('NVM_SYMBLINK', 'C:\lib\nodejs',[System.EnvironmentVariableTarget]::Machine)
+
+# -----------------------------------------------------------------------------
+#
+Write-Host ""
+Write-Host "设置路径PATH" $computerName  -ForegroundColor Yellow
+Write-Host "------------------------------------" -ForegroundColor Green
+$env_reg='Registry::HKEY_LOCAL_MACHINE\System\CurrentControlSet\Control\Session Manager\Environment'
+$default_Path='%USERPROFILE%\AppData\Local\Microsoft\WindowsApps;%USERPROFILE%\.emacs.d\bin;%USERPROFILE%\.doom.d\bin;%USERPROFILE%\go\bin;%USERPROFILE%\.deno\bin;%USERPROFILE%\OneDrive\bin;%NVM_HOME%;%NVM_SYMLINK%;C:\lib\clangd\bin;C:\lib\Neovim\bin;C:\lib\nginx;C:\lib\npc;C:\lib\nvm;C:\lib\PuTTYPortable\App\putty;C:\lib\PuTTYPortable\App\winscp;C:\lib\PuTTYPortable;C:\lib\rsync;C:\Users\Administrator\AppData\Local\nvim\plugged\fzf\bin;C:\lib\mingw\mingw64\bin;C:\lib\heroku\bin;%GOROOT%\bin;%GOPATH%\bin;'
+$originalPath = (Get-ItemProperty -Path $env_reg -Name PATH).path
+$originalPath = $defaultPath + $originalPath + ';'
+
+
+Write-Host "------------------------------------" -ForegroundColor Green
+windows10_env_setup_emacs.ps1
+
+Write-Host "------------------------------------" -ForegroundColor Green
+windows10_env_setup_putty.ps1
+
+Write-Host "------------------------------------" -ForegroundColor Green
+Read-Host -Prompt "安装完成！"
