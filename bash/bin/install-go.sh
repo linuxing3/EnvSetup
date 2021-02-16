d3eb92f1c0f601207f1e785867c40248583a9d26
linuxing3 <linuxing3@qq.com>
2021-02-16 13:56:13 -0400

    minify

--- bash/bin/install-go.sh
+++ bash/bin/install-go.sh
@@ -80,12 +80,18 @@ install_gogs() {
 }
 
 setup_go_env() {
-	if [ ! -d "$HOME/gopath/github.com" ]; then
-		mkdir -p "$HOME/gopath/github.com"
+	if [ -e /usr/lib/go/bin/go ]; then
+		export GOROOT=/usr/lib/go
+	elif [ -e /usr/local/bin/go/bin/go ]; then
+		export GOROOT=/usr/local/bin/go
+	elif [ -e $HOME/.go/bin/go ]; then
+		export GOROOT=$HOME/.go/bin/go
 	fi
-	export GO111MODULE="on"
-	export GOROOT=/usr/lib/go
-	export GOPATH=$HOME/gopath
+	if [ ! -d "$HOME/go/src/github.com" ]; then
+		mkdir -p "$HOME/go/src/github.com"
+	fi
+	export GO111MODULE=on
+	export GOPATH=$HOME/go
 	export PATH=$GOPATH/bin:$GOROOT/bin:$PATH
 	echo "You can also enalbe go environment with ggg!!!"
 }
--- bash/custom/plugins/go/go.plugin.sh
+++ bash/custom/plugins/go/go.plugin.sh
@@ -5,12 +5,14 @@ ggg() {
 		export GOROOT=/usr/lib/go
 	elif [ -e /usr/local/bin/go/bin/go ]; then
 		export GOROOT=/usr/local/bin/go
+	elif [ -e $HOME/.go/bin/go ]; then
+		export GOROOT=$HOME/.go/bin/go
 	fi
-	if [ ! -d "$HOME/gopath/github.com" ]; then
-		mkdir -p "$HOME/gopath/github.com"
+	if [ ! -d "$HOME/go/src/github.com" ]; then
+		mkdir -p "$HOME/go/src/github.com"
 	fi
 	export GO111MODULE=on
-	export GOPATH=$HOME/gopath
+	export GOPATH=$HOME/go
 	export PATH=$GOPATH/bin:$GOROOT/bin:$PATH
 }
 
