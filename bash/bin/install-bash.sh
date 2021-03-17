9e9dbff9e2c93f445ef97fc167c26492d4fd7b26
linuxing3 <linuxing3@qq.com>
2021-03-16 22:04:51 -0400

    a

--- bash/bin/install-bash.sh
+++ bash/bin/install-bash.sh
@@ -10,39 +10,51 @@ cp .bashrc .bashrc.backup
 cp EnvSetup/bash/bashrc.default .bashrc
 
 PS3='Please enter your choice: '
-select opt in "Install" "Skip"; do
+select opt in "Install" "Extra" "bash-it" "Skip"; do
     case $opt in
-        "Install")
+        bash-it)
         echo "==========================================================="
         echo "installing bash-it"
         echo "==========================================================="
+        cd
         if [[ ! -d ~/workspace/bash-it ]]; then
           mkdir -p ~/workspace/bash-it
           cd ~/workspace/bash-it
           git clone https://github.com/Bash-it/bash-it .
         fi
-        cd
+        break
+        ;;
 
+        Install)
+        cd
         tools=$(dialog --title " Bash extra tools 安装自动脚本" \
           --checklist "请输入:" 20 70 5 \
           "ncdu" "Disk usage" 0 \
           "htop" "Process Monitor" 0 \
           "fd-find" "Better finder" 0 \
           "fd" "Other Finder" 0 \
+          "exa" "Modern ls" 0 \
           "rip-grep" "Better grep" 0 \
           3>&1 1>&2 2>&3 3>&1)
         for tool in $tools
         do
           sudo apt install $tool -y
         done
+        break
+        ;;
 
+        Extra)
         echo "==========================================================="
-        echo "Others: like bat"
+        echo "Install bat, Another cat"
         echo "==========================================================="
+        cd
         version=$(dpkg --print-architecture)
         wget https://github.com/sharkdp/bat/releases/download/v0.13.0/bat_0.13.0_${version}.deb
         sudo dpkg -i bat_0.13.0_${version}.deb
         rm bat_0.13.0_${version}*.deb
+
+        echo "Install startship, cross shell prompts"
+        curl -fsSL https://starship.rs/install.sh | bash
 
         echo "==========================================================="
         echo "Others: like prettyping"
@@ -55,8 +67,10 @@ select opt in "Install" "Skip"; do
         echo "Others: like taskbook"
         echo "==========================================================="
         echo "npm install --global taskbook"
+        cd
         break
         ;;
+
         Skip)
         echo "Skipped installing additional tools"
         break
--- bash/custom/aliases.sh
+++ bash/custom/aliases.sh
@@ -26,6 +26,14 @@ alias c='clear'
 alias ll='ls -la'
 # 
 alias l.='ls -d .*'
+if [ "$(command -v exa)" ]; then
+    unalias -m 'll'
+    unalias -m 'l'
+    unalias -m 'la'
+    unalias -m 'ls'
+    alias ls='exa -G  --color auto --icons -a -s type'
+    alias ll='exa -l --color always --icons -a -s type'
+fi
 
 ## a quick way to get out of current directory ##
 alias cd..='cd ..'
